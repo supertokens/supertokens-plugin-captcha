a9a41095a329061603a6130e96d7ade78d4f02ff
Victor Bojica <victor@niftylearning.io>
2025-02-03 02:18:20 +0200

    Update supertokens-plugin-captcha

--- backend/lib/build/plugin.js
+++ backend/lib/build/plugin.js
@@ -1,14 +1,62 @@
 "use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.init = void 0;
 const config_1 = require("./config");
+const axios_1 = __importDefault(require("axios"));
 const init = (config) => {
-    console.log(config);
-    return {
-        id: config_1.PLUGIN_ID,
-        compatibleSDKVersions: config_1.PLUGIN_SDK_VERSION,
-        routeHandlers: [],
-        overrideMap: {},
-    };
+  console.log(config);
+  return {
+    id: config_1.PLUGIN_ID,
+    compatibleSDKVersions: config_1.PLUGIN_SDK_VERSION,
+    routeHandlers: [],
+    overrideMap: {
+      emailpassword: {
+        apis: (originalImplementation) => {
+          if (!originalImplementation.signInPOST) {
+            return originalImplementation;
+          } else {
+            return Object.assign(Object.assign({}, originalImplementation), {
+              signInPOST: async (input) => {
+                var _a;
+                console.log(input);
+                const captcha = "captcha" in input ? input.captcha : null;
+                if (!captcha) {
+                  return {
+                    status: "GENERAL_ERROR",
+                    message: "CAPTCHA verification is required",
+                  };
+                }
+                const result = await axios_1.default.post(
+                  "https://www.google.com/recaptcha/api/siteverify",
+                  {
+                    secret:
+                      (_a = config.reCAPTCHAv3) === null || _a === void 0
+                        ? void 0
+                        : _a.secretKey,
+                    response: captcha,
+                  }
+                );
+                console.log(result.data);
+                if (result.data.success) {
+                  // @ts-ignore
+                  return originalImplementation.signInPOST(input);
+                } else {
+                  return {
+                    status: "GENERAL_ERROR",
+                    message: "CAPTCHA verification failed",
+                  };
+                }
+              },
+            });
+          }
+        },
+      },
+    },
+  };
 };
 exports.init = init;
--- backend/lib/ts/plugin.ts
+++ backend/lib/ts/plugin.ts
@@ -1,6 +1,7 @@
 import { SuperTokensPlugin } from "supertokens-node/types";
 import { PLUGIN_ID, PLUGIN_SDK_VERSION } from "./config";
 import { SuperTokensPluginCaptchaConfig } from "./types";
+import axios from "axios";
 
 export const init = (
   config: SuperTokensPluginCaptchaConfig
@@ -11,6 +12,49 @@ export const init = (
     id: PLUGIN_ID,
     compatibleSDKVersions: PLUGIN_SDK_VERSION,
     routeHandlers: [],
-    overrideMap: {},
+    overrideMap: {
+      emailpassword: {
+        apis: (originalImplementation) => {
+          if (!originalImplementation.signInPOST) {
+            return originalImplementation;
+          } else {
+            return {
+              ...originalImplementation,
+              signInPOST: async (input) => {
+                console.log(input);
+
+                const captcha = "captcha" in input ? input.captcha : null;
+                if (!captcha) {
+                  return {
+                    status: "GENERAL_ERROR",
+                    message: "CAPTCHA verification is required",
+                  };
+                }
+
+                const result = await axios.post(
+                  "https://www.google.com/recaptcha/api/siteverify",
+                  {
+                    secret: config.reCAPTCHAv3?.secretKey,
+                    response: captcha,
+                  }
+                );
+
+                console.log(result.data);
+
+                if (result.data.success) {
+                  // @ts-ignore
+                  return originalImplementation.signInPOST(input);
+                } else {
+                  return {
+                    status: "GENERAL_ERROR",
+                    message: "CAPTCHA verification failed",
+                  };
+                }
+              },
+            };
+          }
+        },
+      },
+    },
   };
 };
